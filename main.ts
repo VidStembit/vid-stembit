ce037242570d487966e839f68ee087994ac48b05
VidStembit <78233740+VidStembit@users.noreply.github.com>
2021-01-29 21:47:56 +0100

    sound

--- main.ts
+++ main.ts
@@ -3,9 +3,19 @@ basic.forever(function () {
         StemBit_Car.CarCtrl(StemBit_Car.CarState.Car_Back)
         basic.pause(1000)
         StemBit_Car.CarCtrl(StemBit_Car.CarState.Car_Right)
+        StemBit_Music.Buzzer(DigitalPin.P0, StemBit_Music.enBuzzer.Beep)
         basic.pause(1000)
     } else {
+        StemBit_Car.Music_Car(StemBit_Car.enMusic.wedding)
         StemBit_Car.CarCtrl(StemBit_Car.CarState.Car_Run)
+        StemBit_Display.RGB(
+        AnalogPin.P0,
+        AnalogPin.P5,
+        AnalogPin.P3,
+        255,
+        0,
+        255
+        )
         basic.pause(1000)
     }
 })
